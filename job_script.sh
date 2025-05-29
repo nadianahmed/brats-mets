e6b8e6e772d688df6ccc61934b4fbcd29317d54a
hudasheikhcu <135059550+hudasheikhcu@users.noreply.github.com>
2025-05-29 19:29:21 -0400

    Update job_script.sh

--- job_script.sh
+++ job_script.sh
@@ -6,17 +6,9 @@
 #SBATCH --time=0-04:00:00
 #SBATCH --output=Huda-%j.out
 
-echo ">> Loading Python module..."
 module load python/3.10
-
-# Activate pre-installed virtual environment
-echo ">> Activating environment..."
 source ~/ENV/bin/activate
 
-echo ">> Moving to project directory..."
 cd /project/def-sreeram/hsheikh1/brats-mets || exit 1
 
-echo ">> Running script on GPU: $CUDA_VISIBLE_DEVICES"
 python -u test_unet.py
-
-echo ">> Job finished."
