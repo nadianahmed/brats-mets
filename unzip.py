6c9c4424621967c6f5858b3a2b0d602b8a2508c4
hudasheikhcu <135059550+hudasheikhcu@users.noreply.github.com>
2025-05-29 22:27:01 -0400

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -2,7 +2,7 @@ import os
 import gzip
 import shutil
 
-base_dir = '/project/def-sreeram/hsheikh1/brats-mets/Datasets/ASNR-MICCAI-BraTS2023-MET-Challenge-TrainingData'
+base_dir = '/project/def-sreeram/hsheikh1/brats-mets/Datasets/ASNR-MICCAI-BraTS2023-MET-Challenge-TrainingData/ASNR-MICCAI-BraTS2023-MET-Challenge-TrainingData'
 
 for root, _, files in os.walk(base_dir):
     for file in files:
