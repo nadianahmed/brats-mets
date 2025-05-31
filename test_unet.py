20e866bc59823a431063faf73900480fa5bc3fe5
hudasheikhcu <135059550+hudasheikhcu@users.noreply.github.com>
2025-05-31 00:10:10 -0400

    Update test_unet.py

--- test_unet.py
+++ test_unet.py
@@ -47,6 +47,42 @@ T2F_TEMPLATE_MATCH_THRESHOLD = 0.6
 T2W_TUMOUR_SIZE = 10
 T2W_TEMPLATE_MATCH_THRESHOLD = 0.6
 
+import torch.nn.functional as F
+
+def crop_or_pad(tensor, target_shape):
+    """
+    Crop or pad a 5D tensor (N, C, D, H, W) to match target shape.
+    """
+    _, _, d, h, w = tensor.shape
+    td, th, tw = target_shape
+
+    # --- Depth (D) ---
+    if d > td:
+        dd = (d - td) // 2
+        tensor = tensor[:, :, dd:dd+td, :, :]
+    elif d < td:
+        pad_d = (td - d)
+        tensor = F.pad(tensor, (0, 0, 0, 0, pad_d // 2, pad_d - pad_d // 2))
+
+    # --- Height (H) ---
+    if h > th:
+        dh = (h - th) // 2
+        tensor = tensor[:, :, :, dh:dh+th, :]
+    elif h < th:
+        pad_h = (th - h)
+        tensor = F.pad(tensor, (0, 0, pad_h // 2, pad_h - pad_h // 2, 0, 0))
+
+    # --- Width (W) ---
+    if w > tw:
+        dw = (w - tw) // 2
+        tensor = tensor[:, :, :, :, dw:dw+tw]
+    elif w < tw:
+        pad_w = (tw - w)
+        tensor = F.pad(tensor, (pad_w // 2, pad_w - pad_w // 2, 0, 0, 0, 0))
+
+    return tensor
+
+
 def get_image_parent_path(path):
     return os.path.dirname(path)
 
